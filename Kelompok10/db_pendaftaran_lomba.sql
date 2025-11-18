1485eba0cd0880efcc6ed7c94e7af778a11ea290
Zahrotun <zahrotun0712@gmail.com>
2025-11-18 11:22:45 +0700

    Add files via upload

---
+++ Kelompok10/db_pendaftaran_lomba.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 18, 2025 at 05:18 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pendaftaran_lomba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pendaftaran`
+--
+
+CREATE TABLE `pendaftaran` (
+  `id_pendaftaran` int(10) UNSIGNED NOT NULL,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `nomor_telepon` varchar(13) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `pilihan_lomba` enum('Nyanyi','Melukis') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ;
+
+--
+-- Dumping data for table `pendaftaran`
+--
+
+INSERT INTO `pendaftaran` (`id_pendaftaran`, `nama_lengkap`, `nomor_telepon`, `email`, `pilihan_lomba`, `created_at`, `updated_at`) VALUES
+(1, 'Andi Setiawan', '0812345678', 'andi@example.com', 'Nyanyi', '2025-11-07 12:04:53', '2025-11-07 12:04:53'),
+(2, 'Siti Rahma', '081234567890', 'siti@example.com', 'Melukis', '2025-11-07 12:04:53', '2025-11-07 12:04:53'),
+(3, 'zahrotun', '0831245678901', '', 'Nyanyi', '2025-11-07 12:09:49', '2025-11-07 13:26:55'),
+(6, 'data', '0987652123321', 'zahrotun0712@gmail.com', 'Nyanyi', '2025-11-13 07:25:26', '2025-11-13 07:25:26');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD PRIMARY KEY (`id_pendaftaran`),
+  ADD KEY `idx_nama_lengkap` (`nama_lengkap`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  MODIFY `id_pendaftaran` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
